76e336a5f8fb9956124063e6237c8acd2c8481a2
Lars Heubach <0346902@student.rocvantwente.nl>
2023-09-26 22:58:41 +0200

    finished product

--- animation.js
+++ animation.js
@@ -1,16 +1,16 @@
 
-//animations index page
+//animations home page
 gsap.from(".logo", { opacity: 0, y: -50, duration: 0.5 });
 gsap.from(".counter", { opacity: 0, y: -50, duration: 0.4, delay: 0.4 });
 gsap.from(".buttonmain", { opacity: 0, y: 50, duration: 0.4, delay: 0.7 });
 gsap.from(".footer1", { opacity: 0, y: 50, duration: 0.3, delay: 0.8});
 
 
-//animations home page
+//animations index page
 gsap.from(".muller", { opacity: 0, duration: 1.2, x: -50, scale: 1, delay: 0.9 });
 gsap.from(".title", { opacity: 0, duration: 1, y: 500 });
 
-//animations home page
+//animations settings page
 gsap.from(".login", { opacity: 0, y: -50, duration: 0.2, delay: 0.5 });
 gsap.from(".fillin", { opacity: 0, y: -50, duration: 0.2, delay: 0.3});
 gsap.from(".submit", { opacity: 0, y: -50, duration: 0.2});
