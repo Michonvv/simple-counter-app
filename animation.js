a6f232696b11b616fe6a8f536415d2790b4c7757
Lars Heubach <0346902@student.rocvantwente.nl>
2023-09-26 20:31:27 +0200

    BOEM GESTYLED

---
+++ animation.js
@@ -0,0 +1,19 @@
+
+//animations index page
+gsap.from(".logo", { opacity: 0, y: -50, duration: 0.5 });
+gsap.from(".counter", { opacity: 0, y: -50, duration: 0.4, delay: 0.4 });
+gsap.from(".buttonmain", { opacity: 0, y: 50, duration: 0.4, delay: 0.7 });
+gsap.from(".footer1", { opacity: 0, y: 50, duration: 0.3, delay: 0.8});
+
+
+//animations home page
+gsap.from(".muller", { opacity: 0, duration: 1.2, x: -50, scale: 1, delay: 0.9 });
+gsap.from(".title", { opacity: 0, duration: 1, y: 500 });
+gsap.from(".footer2", { opacity: 0, y: 50, duration: 0.5, delay: 1.3});
+
+//animations home page
+gsap.from(".login", { opacity: 0, y: -50, duration: 0.2, delay: 0.5 });
+gsap.from(".fillin", { opacity: 0, y: -50, duration: 0.2, delay: 0.3});
+gsap.from(".submit", { opacity: 0, y: -50, duration: 0.2});
+gsap.from(".footer3", { opacity: 0, y: 50, duration: 0.3, delay: 0.5});
+
--- app.js
+++ app.js
@@ -40,3 +40,25 @@
             counterValue = snapshot.val() || 0;
             updateCounter();
         });
+        
+              // button
+      $(function() {
+        $( "#button" ).click(function() {
+          $( "#button" ).addClass( "onclic", 250, validate);
+        });
+      
+        function validate() {
+          setTimeout(function() {
+            $( "#button" ).removeClass( "onclic" );
+            $( "#button" ).addClass( "validate", 450, callback );
+          }, 2250 );
+        }
+          function callback() {
+            setTimeout(function() {
+              $( "#button" ).removeClass( "validate" );
+            }, 1250 );
+          }
+        });
+        
+
+  
